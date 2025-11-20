78ac891d1f61b7cb4507269a79f2d4a80bd69ddf
Kabinan Balakrishnan <balakrishnankabinan@gmail.com>
2025-11-20 13:29:36 +0000

    LongestSubstring completed!

--- HackerEarthPrep/src/main/scala/LongestSubstring.scala
+++ HackerEarthPrep/src/main/scala/LongestSubstring.scala
@@ -1,10 +1,27 @@
 object LongestSubstring {
 
   def lengthOfLongestSubstring(s: String): Int = {
-    0
+    var left = 0
+    var maxlen = 0
+
+    var charSet: Set[Char] = Set()
+
+    for (right <- 0 until s.length) {
+      while (charSet.contains(s(right))) {
+        charSet -= s(left)
+        left += 1
+      }
+      charSet += s(right)
+      maxlen = Math.max(maxlen, right - left + 1)
+    }
+
+    maxlen
   }
 
   def main(args: Array[String]): Unit = {
-
+    println(lengthOfLongestSubstring("abcabcbb"))  // 3
+    println(lengthOfLongestSubstring("bbbbb"))     // 1
+    println(lengthOfLongestSubstring("pwwkew"))    // 3
+    println(lengthOfLongestSubstring(""))          // 0
   }
 }
