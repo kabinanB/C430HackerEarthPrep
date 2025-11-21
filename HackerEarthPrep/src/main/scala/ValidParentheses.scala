dd94be004b045bc364706da8bf3ab43dee8e29fb
Kabinan Balakrishnan <balakrishnankabinan@gmail.com>
2025-11-21 12:04:12 +0000

    valid parentheses done!

---
+++ HackerEarthPrep/src/main/scala/ValidParentheses.scala
@@ -0,0 +1,40 @@
+package DataStructuresClasses
+
+import scala.collection.mutable
+
+object ValidParantheses {
+
+  def isValid(s: String): Boolean = {
+    val stk = mutable.Stack[Char]()
+    val mp = mutable.HashMap(')' -> '(', '}' -> '{', ']' -> '[')
+
+    for (c <- s) {
+      if (mp.contains(c)) { // c is a closing bracket
+        if (stk.nonEmpty && stk.top == mp(c))
+          stk.pop()
+        else
+          return false
+      } else {
+        // c is an opening bracket: push it
+        stk.push(c)
+      }
+    }
+
+    stk.isEmpty
+  }
+
+  def main(args: Array[String]): Unit = {
+    val tests = List(
+      "()" -> true,
+      "()[]{}" -> true,
+      "(]" -> false,
+      "([)]" -> false,
+      "{[]}" -> true
+    )
+
+    for ((input, expected) <- tests) {
+      val result = isValid(input)
+      println(s"Input: $input => Output: $result (Expected: $expected)")
+    }
+  }
+}
